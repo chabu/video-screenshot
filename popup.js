d8d0f317d7ed05a0d5db14569dcf5448d94afb5f
chabu <4394588+chabu@users.noreply.github.com>
2025-04-20 22:36:24 +0900

    動画の再生速度変更機能を追加

--- popup.js
+++ popup.js
@@ -23,6 +23,31 @@ for (const button of buttons) {
 			const filenamePrefixElem = document.querySelector("input[name=filenamePrefix]");
 			filenamePrefixElem.value = "";
 		});
+	} else if (button.name === "videoSpeed") {
+		button.addEventListener("click", async () => {
+			const [tab] = await chrome.tabs.query({
+				active: true,
+				currentWindow: true,
+			});
+
+			await chrome.scripting.executeScript({
+				func: videoSpeed,
+				args: [Number(button.value)],
+				target: {tabId: tab.id},
+			});
+		});
+	} else if (button.name === "injectScript") {
+		button.addEventListener("click", async () => {
+			const [tab] = await chrome.tabs.query({
+				active: true,
+				currentWindow: true,
+			});
+
+			await chrome.scripting.executeScript({
+				files: [button.value],
+				target: {tabId: tab.id},
+			});
+		});
 	}
 }
 
@@ -32,6 +57,17 @@ async function assignStoredValues() {
 	});
 
 	const filenamePrefixElem = document.querySelector("input[name=filenamePrefix]");
-
 	filenamePrefixElem.value = storageLocal.filenamePrefix;
+}
+
+function videoSpeed(delta) {
+	const videos = document.querySelectorAll("video");
+	const strmVideo = Array.from(videos).findLast(
+		(video) => video.currentSrc.startsWith("blob:")
+	);
+	if (delta === 0) {
+		strmVideo.playbackRate = 1.0;
+	} else {
+		strmVideo.playbackRate += delta;
+	}
 }
